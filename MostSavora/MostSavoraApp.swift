14ac931827de4bb7ef2f5deed1f2acc441245b80
ludik <bakhanetssimon@gmail.com>
2026-01-24 20:18:28 +0300

    Initial Commit

---
+++ MostSavora/MostSavoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostSavoraApp.swift
+//  MostSavora
+//
+//  Created by Simon Bakhanets on 24.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MostSavoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MostSavoraTests/MostSavoraTests.swift
@@ -0,0 +1,16 @@
+//
+//  MostSavoraTests.swift
+//  MostSavoraTests
+//
+//  Created by Simon Bakhanets on 24.01.2026.
+//
+
+import Testing
+
+struct MostSavoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
